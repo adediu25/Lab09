22a561e5b60cb98aa489bf46a74e61878100abd3
Alex Dediu <adediu25@gmail.com>
2021-04-04 21:29:57 -0400

    Finished task 3

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,7 @@
 #include "MinHeap.h"
 #include "MaxHeap.h"
 #include <iostream>
+#include <random>
 
 using std::cout;
 using std::endl;
@@ -10,8 +11,8 @@ int main() {
 	// Generate initial heaps for jobs and machines.
 	MinHeap<MachineNode> machineHeap(3);
 	machineHeap.add(MachineNode(1));
-	machineHeap.add(MachineNode(2));
 	machineHeap.add(MachineNode(3));
+	machineHeap.add(MachineNode(2));
 
 	MaxHeap<JobNode> jobHeap(7);
 	jobHeap.add(JobNode(1, 2));
@@ -31,6 +32,36 @@ int main() {
 			<< " to " << currMac.availTime + currJob.time << endl;
 		currMac.availTime += currJob.time;
 		machineHeap.add(currMac);
+	}
+	cout << endl;
+
+	// New set for task 3
+
+	srand(time(0));
+
+	MinHeap<MachineNode> machines(5);
+	machines.add(MachineNode(1));
+	machines.add(MachineNode(2));
+	machines.add(MachineNode(3));
+	machines.add(MachineNode(4));
+	machines.add(MachineNode(5));
+
+	MaxHeap<JobNode> jobs(10);
+
+	for (int i = 1; i <= 10; i++) {
+		int num = rand()%20 + 1;
+		cout << "Job " << i << " time: " << num << endl;
+		jobs.add(JobNode(i, num));
+	}
+
+	while (!jobs.isEmpty()) {
+		JobNode currJob = jobs.extractMax();
+		MachineNode currMac = machines.extractMin();
+		cout << "Schedule job " << currJob.ID << " on machine "
+			<< currMac.ID << " from " << currMac.availTime
+			<< " to " << currMac.availTime + currJob.time << endl;
+		currMac.availTime += currJob.time;
+		machines.add(currMac);
 	}
 
 	return 0;
